eaab13032dafd25f72f96c721c549b9337844a5e
Henry Desroches <yo@henry.codes>
2024-12-28 14:39:22 -0700

    🌐 - Add OG url to templates

--- content/tutorials/tutorials.11tydata.js
+++ content/tutorials/tutorials.11tydata.js
@@ -14,9 +14,18 @@ export default {
       { name: 'Articles', url: '/writing' },
       { name: data.title, url: data.url, isTitle: true },
     ],
+    og: ({ slug }) => {
+      // TODO dev.henry.codes
+      const ogTargetUrl = `https://dev.henry.codes/opengraph/${slug}`;
+      const ogScreenshotUrl = `https://screenshot.henry.codes/${encodeURIComponent(
+        ogTargetUrl
+      )}/opengraph`;
+      console.log(ogScreenshotUrl);
+      return ogScreenshotUrl;
+    },
   },
   hasToc: true,
   layout: 'layouts/_article.njk',
   tags: ['article'],
-  templateEngineOverride: 'md, webc',
+  templateEngineOverride: 'md',
 };
