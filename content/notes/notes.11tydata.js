83969758cea797f0e23886dff701d1382b611e88
Henry Desroches <yo@henry.codes>
2024-12-28 19:05:06 -0700

    🌐 - Add opengraph for notes

--- content/notes/notes.11tydata.js
+++ content/notes/notes.11tydata.js
@@ -1,22 +1,34 @@
-import getReadTime from "../../_11ty/utils/getReadTime.js";
+import getReadTime from '../../_11ty/utils/getReadTime.js';
 
 export default {
   permalink: function ({ title }) {
     return `/writing/${this.slugify(title)}/index.html`;
   },
   hasToc: false,
-  layout: "layouts/_article.njk",
-  tags: ["note"],
-  date: "Last Modified",
+  layout: 'layouts/_article.njk',
+  tags: ['note'],
+  date: 'Last Modified',
   backLink: '/notes',
   backLinkText: 'All notes',
   eleventyComputed: {
     readTime: getReadTime,
     footerLinks: (data) => [
-        ...data.footerLinks,
-        { name: "Articles", url: "/writing" },
-        { name: "Notes", url: "/notes" },
-        { name: data.title, url: data.url, isTitle: true },
+      ...data.footerLinks,
+      { name: 'Articles', url: '/writing' },
+      { name: 'Notes', url: '/notes' },
+      { name: data.title, url: data.url, isTitle: true },
     ],
+    og: function ({ title, slug }) {
+      let slugged = slug;
+      if (!slug) {
+        slugged = this.slugify(title);
+      }
+      // TODO dev.henry.codes
+      const ogTargetUrl = `https://dev.henry.codes/opengraph/writing/${slugged}`;
+      const ogScreenshotUrl = `https://screenshot.henry.codes/${encodeURIComponent(
+        ogTargetUrl
+      )}/opengraph`;
+      return ogScreenshotUrl;
+    },
   },
 };
