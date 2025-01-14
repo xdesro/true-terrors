2679d53245a09eeea00f7d3d18486e885bce162e
Henry Desroches <yo@henry.codes>
2025-01-13 21:14:30 -0700

    ⚙️ - Add links layout toggle just in case

--- src/js/LinksSigil.js
+++ src/js/LinksSigil.js
@@ -6,13 +6,22 @@ gsap.registerPlugin(Observer);
 
 export default class LinksSigil {
   constructor() {
+    this.active = false;
     this.RADIUS = 100;
+    this.actionText = [
+      'This layout isn’t working for me',
+      'No...perform...the ritual.',
+    ];
+
     this.list = document.querySelector('.links-list');
     this.listItems = document.querySelectorAll('.links-list li');
-    this.description = document.querySelector('.links-page-description');
+    this.description = document.querySelector('.links-page__description');
+    this.button = document.querySelector('.links-page__action');
+
+    this.list.classList.add('links-list--radial');
+    this.description.classList.add('links-page__description--positioned');
 
     this.descriptionWidth = this.description.getBoundingClientRect().width;
-    this.list.classList.add('links-list--radial');
     const { width: listWidth, height: listHeight } =
       this.list.getBoundingClientRect();
     const maxListItem = Math.max(
@@ -31,7 +40,12 @@ export default class LinksSigil {
     this.drawPositions();
     this.addListeners();
   }
+  toggleLayoutClasses() {
+    this.description.classList.toggle('links-page__description--positioned');
+    this.list.classList.toggle('links-list--radial');
+  }
   initialDraw() {
+    this.active = true;
     this.listItems.forEach((listItem, i) => {
       const theta = (this.frags / 180) * i * Math.PI;
       //   const x = this.RADIUS * Math.cos(theta);
@@ -56,19 +70,75 @@ export default class LinksSigil {
       target: list,
       type: 'wheel,touch',
       onChange: (self) => {
-        self.event.preventDefault();
+        this.active && self.event.preventDefault();
         this.rotationOffset += self.deltaY;
         this.drawPositions();
       },
     });
     MatchMediaManager.add(({ conditions }) => {
       const { prefersReducedMotion } = conditions;
-
       if (prefersReducedMotion) {
         observer.disable();
       } else {
         observer.enable();
       }
+    });
+
+    this.button.addEventListener('click', () => {
+      this.active = !this.active;
+      console.log('clicked');
+      if (this.active) {
+        observer.enable();
+      } else {
+        observer.disable();
+      }
+      gsap
+        .timeline({
+          onComplete: () => {
+            this.toggleLayoutClasses();
+            this.button.querySelector('span').textContent = this.active
+              ? this.actionText[0]
+              : this.actionText[1];
+            gsap
+              .timeline({ paused: true })
+              .to('.links-list li', {
+                opacity: 1,
+              })
+              .to(
+                '.links-page__description',
+                {
+                  opacity: 1,
+                },
+                '<'
+              )
+              .to(
+                '.links-page__action',
+                {
+                  opacity: 1,
+                },
+                '<'
+              )
+              .play();
+          },
+        })
+        .to('.links-list li', {
+          opacity: 0,
+          stagger: 0.01,
+        })
+        .to(
+          '.links-page__description',
+          {
+            opacity: 0,
+          },
+          '<'
+        )
+        .to(
+          '.links-page__action',
+          {
+            opacity: 0,
+          },
+          '<'
+        );
     });
 
     window.addEventListener('resize', (e) => {
