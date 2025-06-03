eb1e03f97a00a5c71ca54a47f0fc0bf18cf8e65f
Henry Desroches <yo@henry.codes>
2025-06-03 13:29:26 -0600

    🌍 - Fix opengraph url maybe

--- src/content/essays/essays.11tydata.js
+++ src/content/essays/essays.11tydata.js
@@ -24,7 +24,7 @@ export default {
       }/opengraph/writing/${slugged}`;
       const ogScreenshotUrl = `https://screenshot.henry.codes/${encodeURIComponent(
         ogTargetUrl
-      )}/opengraph`;
+      )}`;
       return ogScreenshotUrl;
     },
     meta: function ({ excerpt }) {
