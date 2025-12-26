4eaedd2c565f39f932ff8879e41912b1f4e18a31
Henry Desroches <yo@henry.codes>
2025-12-25 22:24:42 -0700

    🎞️  — Animation for chapter 1 hero and pastoral aside

--- src/custom/a-website-to-destroy/index.js
+++ src/custom/a-website-to-destroy/index.js
@@ -74,3 +74,109 @@ const tocTl = gsap
       scrub: true,
     },
   });
+
+const dividerTl = [...document.querySelectorAll('hr')].map((divider) => {
+  return gsap.from(divider, {
+    scaleX: 0,
+    '--glyph-opacity': 0,
+    scrollTrigger: {
+      trigger: divider,
+      start: 'top bottom-=100px',
+      end: 'top bottom-=300px',
+      scrub: 0.5,
+    },
+  });
+});
+
+const chapterCounterTl = [...document.querySelectorAll('.chapter-counter')].map(
+  (counter) => {
+    return gsap.from(counter, {
+      filter: 'blur(2px)',
+      opacity: 0,
+      scrollTrigger: {
+        trigger: counter,
+        start: 'top bottom-=100px',
+        end: 'top bottom-=300px',
+        scrub: 0.5,
+      },
+    });
+  }
+);
+
+const ch1HeaderTl = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.chapter-one-hero picture',
+      start: 'top bottom',
+      end: 'top center-=100px',
+      scrub: 0.5,
+      once: true,
+    },
+  })
+  .from('.chapter-one-hero picture', {
+    scale: 0.8,
+  })
+  .from(
+    '.chapter-one-hero__title > span:first-child',
+    {
+      y: '1em',
+      filter: 'blur(4px)',
+
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+    },
+    '>-=.3'
+  )
+  .from(
+    '.chapter-one-hero__title > span:last-child',
+    {
+      y: '-1em',
+      filter: 'blur(4px)',
+
+      clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
+    },
+    '<+=.1'
+  )
+  .from(
+    '.chapter-one-hero__title > span:nth-child(2)',
+    {
+      filter: 'blur(4px)',
+      autoAlpha: 0,
+    },
+    '<+=.1'
+  )
+  .from(
+    '.chapter-one-hero figcaption',
+    {
+      autoAlpha: 0,
+    },
+    '<'
+  );
+
+const pastoralTl = gsap
+  .timeline()
+  .fromTo(
+    '.pastoral-aside picture',
+    {
+      yPercent: -10,
+    },
+    {
+      yPercent: 10,
+      scrollTrigger: {
+        trigger: '.pastoral-aside picture',
+        scrub: 0.5,
+        start: 'top bottom',
+        end: 'bottom top',
+      },
+    }
+  )
+  .from('.pastoral-aside__title', {
+    filter: 'blur(4px)',
+    autoAlpha: 0,
+    scrollTrigger: {
+      trigger: '.pastoral-aside__title',
+      scrub: 0.5,
+      once: true,
+      start: 'top bottom-=100px',
+      end: 'top bottom-=300px',
+    },
+  });
