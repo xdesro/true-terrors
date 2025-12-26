2734a66091717f656f513edd0e45b34b4a586738
Henry Desroches <yo@henry.codes>
2025-12-25 21:22:43 -0700

    🗜️  — Test blur scrubbing on TOC for performance

--- src/custom/a-website-to-destroy/index.js
+++ src/custom/a-website-to-destroy/index.js
@@ -7,10 +7,9 @@ import { SplitText } from 'gsap/SplitText';
 
 gsap.registerPlugin(ScrollTrigger, SplitText, CustomEase);
 
-const introTl = gsap.timeline();
 const titleSplit = SplitText.create('.intro__title', { type: 'words' });
-
-introTl
+const introTl = gsap
+  .timeline()
   .from('.intro', {
     opacity: 0,
   })
@@ -27,7 +26,7 @@ introTl
       autoAlpha: 0,
       stagger: 0.05,
     },
-    '>-=.2'
+    '>-=.5'
   )
   .from(
     '.intro__subtitle span',
@@ -42,8 +41,36 @@ introTl
     {
       opacity: 1,
       clipPath: 'polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)',
-      stagger: '.1',
+      stagger: 0.1,
+      xPercent: -2,
       duration: 0.3,
     },
     '-=.3'
   );
+
+const tocTl = gsap
+  .timeline()
+  .from('.toc__title, .toc__list li', {
+    filter: 'blur(2px)',
+    autoAlpha: 0,
+    stagger: 0.1,
+    x: '1rem',
+    scrollTrigger: {
+      trigger: '.toc',
+      start: 'top bottom-=100px',
+      end: 'bottom bottom-=100px',
+      scrub: 0.3,
+    },
+  })
+  .to('.toc__title, .toc__list li', {
+    filter: 'blur(2px)',
+    autoAlpha: 0,
+    stagger: 0.1,
+    x: '-1rem',
+    scrollTrigger: {
+      trigger: '.toc',
+      start: 'top top+=100px',
+      end: 'bottom top+=100px',
+      scrub: true,
+    },
+  });
