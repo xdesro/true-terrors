37b6d925d0dfb4b2530d3fb17bd87dd08cc73aa0
Henry Desroches <yo@henry.codes>
2026-09-06 14:41:14 -0600

    🧡 — Create HN landing page

--- edge-functions/hn.js
+++ edge-functions/hn.js
@@ -1,7 +1,7 @@
 export default async (request, context) => {
   const referer = request.headers.get('referer') || '';
   if (referer.includes('news.ycombinator.com')) {
-    return new Response('Forbidden', { status: 403 });
+    return Response.redirect(new URL('/hn', request.url), 303);
   }
   return context.next();
 };
